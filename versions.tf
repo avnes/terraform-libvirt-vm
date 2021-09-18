a0d8a73c1efe700d5980c189d8bb7a7864480669
Audun Nes <audun.nes@gmail.com>
2021-09-18 22:56:00 +0200

    Use latest and greatest providers

--- versions.tf
+++ versions.tf
@@ -4,28 +4,23 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.6.11"
     }
 
     # TODO: Deprecated: https://registry.terraform.io/providers/hashicorp/template/latest/docs
     template = {
       source = "hashicorp/template"
-      version = "2.2.0"
     }
 
     tls = {
       source = "hashicorp/tls"
-      version = "3.1.0"
     }
 
     local = {
       source = "hashicorp/local"
-      version = "2.1.0"
     }
 
     random = {
       source = "hashicorp/random"
-      version = "3.1.0"
     }
   }
 }
