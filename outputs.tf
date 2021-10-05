29b8850a121324d0a5b6c801a541cfccc0c614ab
Audun Nes <audun.nes@gmail.com>
2021-10-05 22:09:55 +0200

    Simplify module output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,18 @@
-output "hostnames" {
-  value = [libvirt_domain.domain.*]
+output "network" {
+  value = tomap({
+    for k, domain in libvirt_domain.domain : k => domain.network_interface
+  })
 }
 
 output "ansible_password" {
   value     = random_password.password.result
   sensitive = true
+}
+
+output "ssh_private_key_filename" {
+  value     = local_file.ssh_private_key.filename
+}
+
+output "ssh_public_key_filename" {
+  value     = local_file.ssh_public_key.filename
 }
